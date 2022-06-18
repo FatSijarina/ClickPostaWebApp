3af08463248f0104cc589c22d82d1fd9ef64acbb
rrezartHetemi <rrezart.hetemi@outlook.com>
2022-06-18 17:09:14 +0200

    implemented the multipage feature for ordering

---
+++ front-end/src/features/client-interface/make-an-order/confirmation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Confirmation = () => {
+  return (
+    <div>confirmation</div>
+  )
+}
+
+export default Confirmation
--- front-end/src/features/homepage/home-page.jsx
+++ front-end/src/features/homepage/home-page.jsx
@@ -4,26 +4,16 @@ import axios from 'axios';
 
 export default function HomePage() {
 
-  const[klientet, setKlientet] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://localhost:5100/KlientetController/GetKlientet').then(response => {
-      setKlientet(response.data);
-    })
-  }, [])
 
     return (
         <div className="base-container"> 
         
           <h1>This is our main page.</h1>
 
-          <ul>
-          {klientet.map(klienti => (
-              <li key={klienti.id}>
-              {klienti.emri}
-              </li>
-          ))}
-          </ul>
+          
+          <h1>Hello</h1>
+          
          
         </div>    
     )
