08a135e62d63ba8f59511c5d884ac496588546bb
rrezartHetemi <rrezart.hetemi@outlook.com>
2022-06-18 17:20:16 +0200

    git reset

--- front-end/src/features/client-interface/make-an-order/confirmation.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Confirmation = () => {
-  return (
-    <div>confirmation</div>
-  )
-}
-
-export default Confirmation
--- front-end/src/features/client-interface/make-an-order/success.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Success = () => {
-  return (
-    <div>success</div>
-  )
-}
-
-export default Success
